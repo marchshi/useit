6cb6187fe684626ffb88c30991557a3bc66ec87d
marchshi <2356250325@qq.com>
2020-02-10 16:56:28 +0800

    2.10

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,34 +1,18 @@
-// miniprogram/pages/me/me.js
+// miniprogram/pages/search/search.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    avatarUrl: 'images/user-unlogin.png',
-    userInfo: {},
-    logged: false
+
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
+
   },
 
   /**
@@ -77,15 +61,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  },
-
-  /**
-   * 
-   */
-  onGetUserInfo: function (e) {
-
-    console.log(e);
 
   }
 })
