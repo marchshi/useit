e5fda8aa0bdabc23ac57e443a444e24544ace5fb
marchshi <2356250325@qq.com>
2020-03-05 17:31:52 +0800

    2.5

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,6 +8,18 @@ Page({
 
   onLoad: function() {
 
+    wx.cloud.callFunction({
+      name: 'test001',
+      data:{
+        a: 1,
+        b: 2,
+      },
+      success: function(res){
+        console.log(res)
+      },
+      fail:console.error
+    })
+
   },
 
 })
